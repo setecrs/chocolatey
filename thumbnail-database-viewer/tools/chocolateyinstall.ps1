d4045b55ea290599c763f11182e8b557e86926fa
Atila Romero <atilaromero@gmail.com>
2016-04-19 17:12:33 -0300

    thumbnail-database-viewer

---
+++ thumbnail-database-viewer/tools/chocolateyinstall.ps1
@@ -0,0 +1,1 @@
+Install-ChocolateyZipPackage 'thumbnail-database-viewer' 'http://www.itsamples.com/downloads/thumbnail-database-viewer.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
